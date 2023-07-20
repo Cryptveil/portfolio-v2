0a2b9c0379802382a3f342beb6a3804581397baa
Grellheist <grellheist@tutanota.com>
2023-07-20 13:17:46 -0300

    changed colors based on realtimecolors.com

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,16 @@
 module.exports = {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
+        colors: {
+            'primary': '#a38fff',
+            'secondary': '#160033',
+            'accent': '#a879ec',
+            'background': '#050505',
+            'light-primary': '#a38fff',
+            'light-secondary': '#e2ccff',
+            'light-accent': '#7020df',
+            'light-background': '#fafafa',
+        },
         extend: {},
     },
     plugins: [],
