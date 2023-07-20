9634ffe1b35e7cadd1b1abd1dba3d09dfdc7daaf
Grellheist <grellheist@tutanota.com>
2023-07-20 13:00:37 -0300

    changed responsiveness

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,10 +3,10 @@ import { Typewriter } from 'react-simple-typewriter'
 export default function Title() {
     return (
         <div className="flex items-center flex-col select-none">
-            <h1 className='text-2xl xl:text-6xl font-bold'>
+            <h1 className='text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold'>
                 Hey there! I'm Victor Hugo.
             </h1>
-            <span className='text-md xl:text-2xl'>
+            <span className='text-xl md:text-2xl lg:text-3xl xl:text-4xl'>
                 I'm a
                 <span className='font-semibold text-violet-500'>
                     <Typewriter
