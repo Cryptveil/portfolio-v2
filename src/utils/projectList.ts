fbc5ff613a9cf12456f11f67c0450bd3fcbff4c1
Grellheist <grellheist@tutanota.com>
2023-07-30 21:27:56 -0300

    made it better

---
+++ src/utils/projectList.ts
@@ -0,0 +1,26 @@
+import whisker from "/projects/whisker.png"
+
+const projects = [
+    {
+        picture: whisker,
+        alt: "Twitter clone picture",
+        title: "Whisker",
+        description: "Whisker is a social media Website, and a Twitter Clone. It was designed to copy Twitter's layout and design, and it has Tweets (or Meows), Likes and Comments as functionalities. You need to be logged in to see the functionalities.",
+        demo: "https://whisker-network.vercel.app/",
+        source: "https://github.com/Grellheist/whisker",
+        technologies: [
+            "TailwindCSS",
+            "NextJS",
+            "Typescript",
+            "Clerk",
+            "Prisma",
+            "MySQL",
+            "RadixUI",
+            "Vercel",
+            "PlanetScale",
+        ],
+        account: { username: "demo123", password: "Jj@7TsFk" }
+    },
+]
+
+export default projects
